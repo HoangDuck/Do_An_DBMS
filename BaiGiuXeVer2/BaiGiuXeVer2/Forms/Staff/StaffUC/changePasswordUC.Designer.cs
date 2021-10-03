fd3f5d0839e24a1749fb8ad760cc7a6c81923e87
Văn Tính Bùi <vantinh101@gmail.com>
2021-10-03 12:25:17 +0700

    update event change password

--- BaiGiuXeVer2/BaiGiuXeVer2/Forms/Staff/StaffUC/changePasswordUC.Designer.cs
+++ BaiGiuXeVer2/BaiGiuXeVer2/Forms/Staff/StaffUC/changePasswordUC.Designer.cs
@@ -30,12 +30,12 @@ namespace BaiGiuXeVer2.Forms.Staff.StaffUC
         private void InitializeComponent()
         {
             this.gunaShadowPanel1 = new Guna.UI.WinForms.GunaShadowPanel();
-            this.label1 = new System.Windows.Forms.Label();
+            this.btnChange = new Guna.UI.WinForms.GunaButton();
             this.cbShowPassword = new Guna.UI.WinForms.GunaCheckBox();
             this.textConfirmNewPassword = new Guna.UI.WinForms.GunaLineTextBox();
-            this.textNewPassword = new Guna.UI.WinForms.GunaLineTextBox();
             this.textCurrentPassword = new Guna.UI.WinForms.GunaLineTextBox();
-            this.btnChange = new Guna.UI.WinForms.GunaButton();
+            this.textNewPassword = new Guna.UI.WinForms.GunaLineTextBox();
+            this.label1 = new System.Windows.Forms.Label();
             this.gunaShadowPanel1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -57,81 +57,11 @@ namespace BaiGiuXeVer2.Forms.Staff.StaffUC
             this.gunaShadowPanel1.Size = new System.Drawing.Size(467, 718);
             this.gunaShadowPanel1.TabIndex = 0;
             // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Leelawadee UI", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
-            this.label1.Location = new System.Drawing.Point(92, 95);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(278, 50);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "ĐỔI MẬT KHẨU";
-            // 
-            // cbShowPassword
-            // 
-            this.cbShowPassword.BaseColor = System.Drawing.Color.White;
-            this.cbShowPassword.CheckedOffColor = System.Drawing.Color.Gray;
-            this.cbShowPassword.CheckedOnColor = System.Drawing.Color.FromArgb(((int)(((byte)(100)))), ((int)(((byte)(88)))), ((int)(((byte)(255)))));
-            this.cbShowPassword.FillColor = System.Drawing.Color.White;
-            this.cbShowPassword.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.cbShowPassword.Location = new System.Drawing.Point(21, 382);
-            this.cbShowPassword.Name = "cbShowPassword";
-            this.cbShowPassword.Size = new System.Drawing.Size(150, 22);
-            this.cbShowPassword.TabIndex = 84;
-            this.cbShowPassword.Text = "Hiển thị mật khẩu";
-            // 
-            // textConfirmNewPassword
-            // 
-            this.textConfirmNewPassword.BackColor = System.Drawing.Color.White;
-            this.textConfirmNewPassword.Cursor = System.Windows.Forms.Cursors.IBeam;
-            this.textConfirmNewPassword.FocusedLineColor = System.Drawing.Color.FromArgb(((int)(((byte)(100)))), ((int)(((byte)(88)))), ((int)(((byte)(255)))));
-            this.textConfirmNewPassword.Font = new System.Drawing.Font("Yu Gothic UI Light", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textConfirmNewPassword.LineColor = System.Drawing.Color.Gainsboro;
-            this.textConfirmNewPassword.Location = new System.Drawing.Point(21, 309);
-            this.textConfirmNewPassword.Name = "textConfirmNewPassword";
-            this.textConfirmNewPassword.PasswordChar = '\0';
-            this.textConfirmNewPassword.SelectedText = "";
-            this.textConfirmNewPassword.Size = new System.Drawing.Size(425, 37);
-            this.textConfirmNewPassword.TabIndex = 83;
-            this.textConfirmNewPassword.Text = "Confirm New Password";
-            this.textConfirmNewPassword.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
-            // 
-            // textNewPassword
-            // 
-            this.textNewPassword.BackColor = System.Drawing.Color.White;
-            this.textNewPassword.Cursor = System.Windows.Forms.Cursors.IBeam;
-            this.textNewPassword.FocusedLineColor = System.Drawing.Color.FromArgb(((int)(((byte)(100)))), ((int)(((byte)(88)))), ((int)(((byte)(255)))));
-            this.textNewPassword.Font = new System.Drawing.Font("Yu Gothic UI Light", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textNewPassword.LineColor = System.Drawing.Color.Gainsboro;
-            this.textNewPassword.Location = new System.Drawing.Point(21, 249);
-            this.textNewPassword.Name = "textNewPassword";
-            this.textNewPassword.PasswordChar = '\0';
-            this.textNewPassword.SelectedText = "";
-            this.textNewPassword.Size = new System.Drawing.Size(425, 37);
-            this.textNewPassword.TabIndex = 82;
-            this.textNewPassword.Text = "New Password";
-            this.textNewPassword.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
-            // 
-            // textCurrentPassword
-            // 
-            this.textCurrentPassword.BackColor = System.Drawing.Color.White;
-            this.textCurrentPassword.Cursor = System.Windows.Forms.Cursors.IBeam;
-            this.textCurrentPassword.FocusedLineColor = System.Drawing.Color.FromArgb(((int)(((byte)(100)))), ((int)(((byte)(88)))), ((int)(((byte)(255)))));
-            this.textCurrentPassword.Font = new System.Drawing.Font("Yu Gothic UI Light", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textCurrentPassword.LineColor = System.Drawing.Color.Gainsboro;
-            this.textCurrentPassword.Location = new System.Drawing.Point(21, 195);
-            this.textCurrentPassword.Name = "textCurrentPassword";
-            this.textCurrentPassword.PasswordChar = '\0';
-            this.textCurrentPassword.SelectedText = "";
-            this.textCurrentPassword.Size = new System.Drawing.Size(425, 37);
-            this.textCurrentPassword.TabIndex = 82;
-            this.textCurrentPassword.Text = "Current Password";
-            this.textCurrentPassword.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
-            // 
             // btnChange
             // 
             this.btnChange.AnimationHoverSpeed = 0.07F;
             this.btnChange.AnimationSpeed = 0.03F;
+            this.btnChange.BackColor = System.Drawing.Color.Transparent;
             this.btnChange.BaseColor = System.Drawing.Color.White;
             this.btnChange.BorderColor = System.Drawing.Color.Black;
             this.btnChange.BorderSize = 3;
@@ -153,6 +83,84 @@ namespace BaiGiuXeVer2.Forms.Staff.StaffUC
             this.btnChange.TabIndex = 85;
             this.btnChange.Text = "Đổi";
             this.btnChange.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // cbShowPassword
+            // 
+            this.cbShowPassword.BaseColor = System.Drawing.Color.White;
+            this.cbShowPassword.CheckedOffColor = System.Drawing.Color.Gray;
+            this.cbShowPassword.CheckedOnColor = System.Drawing.Color.FromArgb(((int)(((byte)(100)))), ((int)(((byte)(88)))), ((int)(((byte)(255)))));
+            this.cbShowPassword.FillColor = System.Drawing.Color.White;
+            this.cbShowPassword.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cbShowPassword.Location = new System.Drawing.Point(21, 382);
+            this.cbShowPassword.Name = "cbShowPassword";
+            this.cbShowPassword.Size = new System.Drawing.Size(150, 22);
+            this.cbShowPassword.TabIndex = 84;
+            this.cbShowPassword.Text = "Hiển thị mật khẩu";
+            this.cbShowPassword.CheckedChanged += new System.EventHandler(this.cbShowPassword_CheckedChanged);
+            // 
+            // textConfirmNewPassword
+            // 
+            this.textConfirmNewPassword.BackColor = System.Drawing.Color.White;
+            this.textConfirmNewPassword.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.textConfirmNewPassword.FocusedLineColor = System.Drawing.Color.FromArgb(((int)(((byte)(100)))), ((int)(((byte)(88)))), ((int)(((byte)(255)))));
+            this.textConfirmNewPassword.Font = new System.Drawing.Font("Yu Gothic UI Light", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textConfirmNewPassword.LineColor = System.Drawing.Color.Gainsboro;
+            this.textConfirmNewPassword.Location = new System.Drawing.Point(21, 309);
+            this.textConfirmNewPassword.Name = "textConfirmNewPassword";
+            this.textConfirmNewPassword.PasswordChar = '\0';
+            this.textConfirmNewPassword.SelectedText = "";
+            this.textConfirmNewPassword.Size = new System.Drawing.Size(425, 37);
+            this.textConfirmNewPassword.TabIndex = 83;
+            this.textConfirmNewPassword.Text = "Confirm New Password";
+            this.textConfirmNewPassword.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textConfirmNewPassword.Enter += new System.EventHandler(this.textConfirmNewPassword_Enter);
+            this.textConfirmNewPassword.Leave += new System.EventHandler(this.textConfirmNewPassword_Leave);
+            // 
+            // textCurrentPassword
+            // 
+            this.textCurrentPassword.BackColor = System.Drawing.Color.White;
+            this.textCurrentPassword.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.textCurrentPassword.FocusedLineColor = System.Drawing.Color.FromArgb(((int)(((byte)(100)))), ((int)(((byte)(88)))), ((int)(((byte)(255)))));
+            this.textCurrentPassword.Font = new System.Drawing.Font("Yu Gothic UI Light", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textCurrentPassword.LineColor = System.Drawing.Color.Gainsboro;
+            this.textCurrentPassword.Location = new System.Drawing.Point(21, 195);
+            this.textCurrentPassword.Name = "textCurrentPassword";
+            this.textCurrentPassword.PasswordChar = '\0';
+            this.textCurrentPassword.SelectedText = "";
+            this.textCurrentPassword.Size = new System.Drawing.Size(425, 37);
+            this.textCurrentPassword.TabIndex = 82;
+            this.textCurrentPassword.Text = "Current Password";
+            this.textCurrentPassword.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textCurrentPassword.Enter += new System.EventHandler(this.textCurrentPassword_Enter);
+            this.textCurrentPassword.Leave += new System.EventHandler(this.textCurrentPassword_Leave);
+            // 
+            // textNewPassword
+            // 
+            this.textNewPassword.BackColor = System.Drawing.Color.White;
+            this.textNewPassword.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.textNewPassword.FocusedLineColor = System.Drawing.Color.FromArgb(((int)(((byte)(100)))), ((int)(((byte)(88)))), ((int)(((byte)(255)))));
+            this.textNewPassword.Font = new System.Drawing.Font("Yu Gothic UI Light", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textNewPassword.LineColor = System.Drawing.Color.Gainsboro;
+            this.textNewPassword.Location = new System.Drawing.Point(21, 249);
+            this.textNewPassword.Name = "textNewPassword";
+            this.textNewPassword.PasswordChar = '\0';
+            this.textNewPassword.SelectedText = "";
+            this.textNewPassword.Size = new System.Drawing.Size(425, 37);
+            this.textNewPassword.TabIndex = 82;
+            this.textNewPassword.Text = "New Password";
+            this.textNewPassword.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textNewPassword.Enter += new System.EventHandler(this.textNewPassword_Enter);
+            this.textNewPassword.Leave += new System.EventHandler(this.textNewPassword_Leave);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Leelawadee UI", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.label1.Location = new System.Drawing.Point(92, 95);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(278, 50);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "ĐỔI MẬT KHẨU";
             // 
             // changePasswordUC
             // 
--- BaiGiuXeVer2/BaiGiuXeVer2/Forms/Staff/StaffUC/changePasswordUC.cs
+++ BaiGiuXeVer2/BaiGiuXeVer2/Forms/Staff/StaffUC/changePasswordUC.cs
@@ -16,5 +16,105 @@ namespace BaiGiuXeVer2.Forms.Staff.StaffUC
         {
             InitializeComponent();
         }
+
+        private void textCurrentPassword_Enter(object sender, EventArgs e)
+        {
+            if (textCurrentPassword.Text == "Current Password")
+            {
+                if (!cbShowPassword.Checked)
+                {
+                    textCurrentPassword.UseSystemPasswordChar = true;
+                }
+                else
+                {
+                    textCurrentPassword.UseSystemPasswordChar = false;
+                }
+                textCurrentPassword.Text = "";
+            }
+        }
+
+        private void textCurrentPassword_Leave(object sender, EventArgs e)
+        {
+            if (textCurrentPassword.Text == "")
+            {
+                textCurrentPassword.UseSystemPasswordChar = false;
+                textCurrentPassword.Text = "Current Password";
+            }
+        }
+
+        private void textNewPassword_Enter(object sender, EventArgs e)
+        {
+            if (textNewPassword.Text == "New Password")
+            {
+                if (!cbShowPassword.Checked)
+                {
+                    textNewPassword.UseSystemPasswordChar = true;
+                }
+                else
+                {
+                    textNewPassword.UseSystemPasswordChar = false;
+                }
+                textNewPassword.Text = "";
+            }
+        }
+
+        private void textNewPassword_Leave(object sender, EventArgs e)
+        {
+            if (textNewPassword.Text == "")
+            {
+                textNewPassword.UseSystemPasswordChar = false;
+                textNewPassword.Text = "New Password";
+            }
+        }
+
+        private void textConfirmNewPassword_Enter(object sender, EventArgs e)
+        {
+            if (textConfirmNewPassword.Text == "Confirm New Password")
+            {
+                if (!cbShowPassword.Checked)
+                {
+                    textConfirmNewPassword.UseSystemPasswordChar = true;
+                }
+                else
+                {
+                    textConfirmNewPassword.UseSystemPasswordChar = false;
+                }
+                textConfirmNewPassword.Text = "";
+            }
+        }
+
+        private void textConfirmNewPassword_Leave(object sender, EventArgs e)
+        {
+            if (textConfirmNewPassword.Text == "")
+            {
+                textConfirmNewPassword.UseSystemPasswordChar = false;
+                textConfirmNewPassword.Text = "Confirm New Password";
+            }
+        }
+
+        private void cbShowPassword_CheckedChanged(object sender, EventArgs e)
+        {
+            if (cbShowPassword.Checked)
+            {
+                textCurrentPassword.UseSystemPasswordChar = false;
+                textNewPassword.UseSystemPasswordChar = false;
+                textConfirmNewPassword.UseSystemPasswordChar = false;
+            }
+            else
+            {
+                if (textCurrentPassword.Text != "Current Password")
+                {
+                    textCurrentPassword.UseSystemPasswordChar = true;
+                }
+                if (textNewPassword.Text != "New Password")
+                {
+                    textNewPassword.UseSystemPasswordChar = true;
+                }
+                if (textConfirmNewPassword.Text != "Confirm New Password")
+                {
+                    textConfirmNewPassword.UseSystemPasswordChar = true;
+                }
+            }
+        }
     }
 }
